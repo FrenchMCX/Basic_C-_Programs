e8b12df6a38bcbb753fc55022be8b009f8ad39e0
Tyler French <81813288+FrenchMCX@users.noreply.github.com>
2022-09-23 11:49:42 -0400

    .

---
+++ MethodAssignment/Methods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    internal class Methods
+    {
+
+        public int addSeven(int input1, int input2 = 0) 
+        {
+            int result = input1 + input2 + 7;
+        
+          return result;
+        }
+
+    }
+}
---
+++ MethodAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Try and Catch used to prevent program from not working when nothing is entered for second input.
+            try
+            {
+                //Instantiate methods
+                Methods methods = new Methods();
+
+                //Ask for and receive inputs. Note to use zero if they desire to enter no number for second input.
+                Console.WriteLine("Insert up to 2 numbers to add with 7. (If you wish to only use 1 number please enter zero for the second.)");
+                int input1 = Convert.ToInt32(Console.ReadLine());
+
+                int input2 = Convert.ToInt32(Console.ReadLine());
+
+                //Call methods
+                int result = methods.addSeven(input1, input2);
+
+                //Print results
+                Console.WriteLine(result);
+            }
+            catch (FormatException ex) { Console.WriteLine(ex.Message); }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
