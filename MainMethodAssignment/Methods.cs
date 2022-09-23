960d27233354a853b2acf04583d9254ee73c729e
Tyler French <81813288+FrenchMCX@users.noreply.github.com>
2022-09-23 11:18:49 -0400

    .

---
+++ MainMethodAssignment/Methods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    internal class Methods
+    {
+        //method to add 7 to input
+        public int addSeven(int input) { int result = input + 7; return result; }
+        //method to add 7.7 to input
+        public double addSeven(double input) { double result = input + 7.7; return (int)result; }
+        //method to add 7 to input
+        public string addSeven(string input) { string result = input + 7; return result; }
+    }
+}
---
+++ MainMethodAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate methods
+            Methods methods = new Methods();
+
+            //Ask for input
+            Console.WriteLine("Insert a number to add 7 to, a decimal to add 7.7 to, and a string to add 7 to.");
+            int input = Convert.ToInt32(Console.ReadLine());
+            double input1 = Convert.ToDouble(Console.ReadLine());
+            string input2 = Convert.ToString(Console.ReadLine());
+            //Call methods
+            int result1 = methods.addSeven(input);
+            double result2 = methods.addSeven(input1);
+            string result3 = methods.addSeven(input2);
+
+            //Print results
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+
+            Console.ReadLine();
+        }
+    }
+}
