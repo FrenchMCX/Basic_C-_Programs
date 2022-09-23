4d3f70bb4f3a8a626da93bb89e0a24104e35069c
Tyler French <81813288+FrenchMCX@users.noreply.github.com>
2022-09-23 12:40:59 -0400

    .

---
+++ MethodClassAssignment/Methods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    internal class Methods
+    {
+        //create method that returns second parameter
+        public void addSeven(int input1, int input2) { int result = input1 + 7; Console.WriteLine(input2); }
+    }
+}
---
+++ MethodClassAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate methods
+            Methods methods = new Methods();
+            int input1 = 1;
+            int input2 = 2;
+
+            //Call method
+            methods.addSeven(1, 2);
+            methods.addSeven(input1, input2);
+
+            Console.ReadLine();
+        }
+    }
+}
