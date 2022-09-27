86a3390dd131e150ad53549b49b052847452a9f4
Tyler French <81813288+FrenchMCX@users.noreply.github.com>
2022-09-27 15:32:57 -0400

    Create EnumAssignment.cs

---
+++ EnumAssignment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week: ");
+                string DayOfTheWeekInput = Console.ReadLine();
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), DayOfTheWeekInput);
+                Console.WriteLine("Yes, today is " + day);
+            }
+            catch(ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            } 
+
+            Console.ReadLine();
+        }
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
