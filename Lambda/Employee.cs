9f38d2429f3205e5e8b0481007feba09707eec54
Tyler French <81813288+FrenchMCX@users.noreply.github.com>
2022-09-27 19:38:56 -0400

    .

---
+++ Lambda/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lambda
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string fName { get; set; }
+        public string lName { get; set; }
+    }
+}
--- TwentyOne/Player.cs
+++ TwentyOne/Player.cs
@@ -6,11 +6,30 @@ using System.Threading.Tasks;
 
 namespace TwentyOne
 {
-    internal class Player
+    public class Player
     {
+        public Player(string name, int beginningBalance) 
+        {
+            Hand = new List<Card>();
+            Balance = beginningBalance;
+            Name = name;
+        }
+
         public List<Card> Hand { get; set; }
         public int Balance { get; set; }
         public string Name { get; set; }
         public bool isActivelyPlaying { get; set; }
+
+        public static Game operator +(Game game, Player player) 
+        {
+            game.Players.Add(player);
+            return game;
+        }
+
+        public static Game operator -(Game game, Player player)
+        {
+            game.Players.Remove(player);
+            return game;
+        }
     }
 }
