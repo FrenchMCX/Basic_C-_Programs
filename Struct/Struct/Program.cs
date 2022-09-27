ca92fe5ea479b6d9532fc8bec10dba878a275352
Tyler French <81813288+FrenchMCX@users.noreply.github.com>
2022-09-27 16:14:11 -0400

    .

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Intantiate Number
+            Number number = new Number();
+            //Set Value
+            number.x = 35;
+            //Print
+            Console.WriteLine(number.x);
+            Console.ReadLine();
+        }
+
+        //Create Struct
+        struct Number
+        {
+            public int x;
+        }
+    }
+}
